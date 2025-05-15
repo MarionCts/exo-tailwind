5d3389b7541bb0010880f6a1c73a7e274a635b3a
Marion Cts <104251209+MarionCts@users.noreply.github.com>
2025-05-15 13:30:58 +0200

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+function mobileClose() {
+    const el = document.getElementById("closeButton");
+    if (el.style.display === "none") {
+        el.style.display = "block";
+    } else {
+        el.style.display = "none";
+    }
+}
+
+addEventListener("toggle", (mobileClose) => { })
+
+ontoggle = (mobileClose) => { }
